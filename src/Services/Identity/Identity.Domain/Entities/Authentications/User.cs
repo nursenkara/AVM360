9bb3e081b1aea1fcc412483587f1f00eba234d54
Nur Şenkara <nsenkara19@gmail.com>
2022-06-27 11:01:38 +0300

    init

---
+++ src/Services/Identity/Identity.Domain/Entities/Authentications/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Domain.Entities.Authentications
+{
+    public class User : IdentityUser<int>
+    {
+        public User()
+        {
+
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime LastLoginDate { get; set; }
+    }
+}
